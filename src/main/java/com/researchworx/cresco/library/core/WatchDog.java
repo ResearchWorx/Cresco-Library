d81809a8beec4bca5855385f234978e577d0f2ff
Caylin Hickey <caylin.hickey@uky.edu>
2016-04-17 23:42:20 -0400

    Added annotations Moved start to separate method Reformatted Tick method

--- src/main/java/com/researchworx/cresco/library/core/WatchDog.java
+++ src/main/java/com/researchworx/cresco/library/core/WatchDog.java
@@ -6,83 +6,127 @@ import com.researchworx.cresco.library.utilities.CLogger;
 import java.util.Timer;
 import java.util.TimerTask;
 
+/**
+ * Cresco WatchDog heartbeat system
+ * @author V.K. Cody Bumgardner
+ * @author Caylin Hickey
+ * @version 0.2.7
+ */
 public class WatchDog {
+    /** Timer control object */
     private Timer timer;
+    /** Control object */
     private boolean running = false;
-    private long interval;
+    /** Start time of the WatchDog timer */
     private long startTS;
-
+    /** Region to report from */
     private String region;
+    /** Agent to report from */
     private String agent;
-    private String plugin;
-    private CLogger clog;
+    /** Plugin ID to report from */
+    private String pluginID;
+    /** Logger instance */
+    private CLogger logger;
+    /** Configuration instance */
     private Config config;
 
-    public WatchDog(String region, String agent, String plugin, CLogger clog, Config config) {
+    /**
+     * Constructor
+     * @param region        Region to report from
+     * @param agent         Agent to report from
+     * @param pluginID      Plugin ID to report from
+     * @param logger        Logger instance
+     * @param config        Configuration instance
+     */
+    public WatchDog(String region, String agent, String pluginID, CLogger logger, Config config) {
         this.region = region;
         this.agent = agent;
-        this.plugin = plugin;
-        this.clog = clog;
+        this.pluginID = pluginID;
+        this.logger = logger;
         this.config = config;
-        this.interval = config.getLongParam("", "watchdogtimer", 5000L);
-        startTS = System.currentTimeMillis();
-
-        MsgEvent initial = new MsgEvent(MsgEvent.Type.INFO, this.region, this.agent, this.plugin, "WatchDog timer set to " + this.interval + " milliseconds");
-        initial.setParam("src_region", this.region);
-        initial.setParam("src_agent", this.agent);
-        initial.setParam("src_plugin", this.plugin);
-        initial.setParam("dst_region", this.region);
-        this.clog.log(initial);
-
-        start();
     }
 
-    private class WatchDogTask extends TimerTask {
-        private String region;
-        private String agent;
-        private String plugin;
-        private Long interval;
-        private CLogger clog;
-
-        WatchDogTask(String region, String agent, String plugin, Long interval, CLogger clog) {
-            this.region = region;
-            this.agent = agent;
-            this.plugin = plugin;
-            this.interval = interval;
-            this.clog = clog;
-        }
-
-        public void run() {
-            long runTime = System.currentTimeMillis() - startTS;
-            MsgEvent tick = new MsgEvent(MsgEvent.Type.WATCHDOG, this.region, null, null, "WatchDog timer set to " + this.interval + " milliseconds");
-            tick.setParam("src_region", this.region);
-            tick.setParam("src_agent", this.agent);
-            tick.setParam("src_plugin", this.plugin);
-            tick.setParam("dst_region", this.region);
-            tick.setParam("runtime", String.valueOf(runTime));
-            tick.setParam("timestamp", String.valueOf(System.currentTimeMillis()));
-            this.clog.log(tick);
-        }
-    }
-
+    /**
+     * Starts this WatchDog instance
+     * @return              This instance
+     */
     public WatchDog start() {
         if (this.running) return this;
-        this.interval = this.config.getLongParam("", "watchdogtimer", 5000L);
+        Long interval = config.getLongParam("", "watchdogtimer", 5000L);
+        this.startTS = System.currentTimeMillis();
+
+        MsgEvent initial = new MsgEvent(MsgEvent.Type.INFO, this.region, this.agent, this.pluginID, "WatchDog timer set to " + interval + " milliseconds");
+        initial.setParam("src_region", this.region);
+        initial.setParam("src_agent", this.agent);
+        initial.setParam("src_plugin", this.pluginID);
+        initial.setParam("dst_region", this.region);
+        this.logger.log(initial);
+
         this.timer = new Timer();
-        this.timer.scheduleAtFixedRate(new WatchDogTask(this.region, this.agent, this.plugin, this.interval, this.clog), 500, this.interval);
+        this.timer.scheduleAtFixedRate(new WatchDogTask(this.region, this.agent, this.pluginID, this.logger), 500, interval);
         return this;
     }
 
-    public void restart() {
+    /**
+     * Restarts this WatchDog instance
+     * @return              This instance
+     */
+    public WatchDog restart() {
         if (this.running) this.timer.cancel();
         this.running = false;
-        start();
+        return start();
     }
 
+    /**
+     * Stops this WatchDog instance
+     * @return              Whether the instance was stopped
+     */
     public boolean stop() {
         if (!this.running) return false;
         this.timer.cancel();
         this.running = false;
         return true;
+    }
+
+    /**
+     * Tick of the WatchDog timer
+     */
+    private class WatchDogTask extends TimerTask {
+        /** Region to report from */
+        private String region;
+        /** Agent to report from */
+        private String agent;
+        /** Plugin ID to report from */
+        private String pluginID;
+        /** Logger channel */
+        private CLogger logger;
+
+        /**
+         * Constructor
+         * @param region        Region to report from
+         * @param agent         Agent to report from
+         * @param pluginID      Plugin ID to report from
+         * @param logger        Logger channel
+         */
+        WatchDogTask(String region, String agent, String pluginID, CLogger logger) {
+            this.region = region;
+            this.agent = agent;
+            this.pluginID = pluginID;
+            this.logger = logger;
+        }
+
+        /**
+         * Tick
+         */
+        public void run() {
+            MsgEvent tick = new MsgEvent(MsgEvent.Type.WATCHDOG, this.region, null, null, "WatchDog timer tick.");
+            tick.setParam("src_region", this.region);
+            tick.setParam("src_agent", this.agent);
+            tick.setParam("src_plugin", this.pluginID);
+            tick.setParam("dst_region", this.region);
+            tick.setParam("runtime", String.valueOf(System.currentTimeMillis() - startTS));
+            tick.setParam("timestamp", String.valueOf(System.currentTimeMillis()));
+            this.logger.log(tick);
+        }
     }
 }
